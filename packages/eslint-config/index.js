1f5f4c7d7e4009a6643b80c5877013a6e81c0f22
OrzMiku <miku@ecy.pink>
2025-07-03 09:14:13 +0800

    feat(database): 添加 Prettier 和 ESLint 配置，更新依赖项为 catalog 形式

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+  {
+    ignores: ['**/dist/**', '**/build/**', '**/node_modules/**', '**/.next/**'],
+  },
   js.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
