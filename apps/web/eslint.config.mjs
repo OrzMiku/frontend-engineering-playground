a1e835d072199e49e7c902c5f73dfd1212fae80e
OrzMiku <miku@ecy.pink>
2025-07-04 12:23:45 +0800

    feat(config): 更新 ESLint 配置，调整组件入口文件路径，添加 tsconfig 扩展

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
-import eslintConfigPrettier from '@qqmc/eslint-config';
 
-export default withNuxt(eslintConfigPrettier);
+export default withNuxt();
