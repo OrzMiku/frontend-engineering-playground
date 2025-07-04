190a76d4f2661b43ab6f5aaab9a9fec601359b98
OrzMiku <miku@ecy.pink>
2025-07-04 10:10:25 +0800

    feat(web): 添加 tailwindcss

---
+++ apps/web/layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="bg-white dark:bg-gray-900 dark:text-gray-50 text-gray-900 min-h-screen"
+  >
+    <NuxtRouteAnnouncer />
+    <slot />
+  </div>
+</template>
---
+++ apps/web/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'Hello World',
+});
+const msg = 'Hello World!';
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-50 text-4xl"
+  >
+    {{ msg }}
+  </div>
+</template>
