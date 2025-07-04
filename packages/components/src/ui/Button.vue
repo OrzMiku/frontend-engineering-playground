65d1aa2612104a939c8563d886efe11743c0b18b
OrzMiku <miku@ecy.pink>
2025-07-04 10:46:17 +0800

    feat(components): 组件库初始化

---
+++ packages/components/src/ui/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button :type="type || 'button'" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
+}>();
+</script>
